7f7dc7135e054e1090366ca282c4c3edca7674a3
7b809 <ej818793@gmail.com>
2026-03-05 01:28:54 +0530

    Auto update

--- start.sh
+++ start.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 PORT=${PORT:-8080}
-PASSWORD=${PASSWORD:-123456}
 
-echo "Server password is: $PASSWORD"
-echo "Type of server password: $(echo "$PASSWORD" | awk '{print typeof($0)}')"
-
-echo "Enter password to access server:"
-read USER_PASSWORD
-
-echo "User entered password: $USER_PASSWORD"
-echo "Type of user password: $(echo "$USER_PASSWORD" | awk '{print typeof($0)}')"
-
-if [ "$USER_PASSWORD" = "$PASSWORD" ]; then
-    echo "Password matched. Starting code-server..."
-else
-    echo "Password does not match."
-fi
+echo "Starting code-server without authentication..."
+echo "Server running on port: $PORT"
 
 code-server \
 --bind-addr 0.0.0.0:$PORT \
---auth password \
---password $PASSWORD \
+--auth none \
 /home/coder/project
