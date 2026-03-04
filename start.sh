2c4a831048eb3f0a9816b0c17b72de302655b7e9
7b809 <ej818793@gmail.com>
2026-03-05 01:59:28 +0530

    Auto update

--- start.sh
+++ start.sh
@@ -8,7 +8,4 @@ echo "=================================="
 
 # Use --auth none to disable the password prompt.
 # We remove the "FROM codercom..." text as that belongs in the Dockerfile.
-exec code-server \
-  --bind-addr 0.0.0.0:$PORT \
-  --auth none \
-  /home/coder/project
+exec code-server --bind-addr 0.0.0.0:$PORT --auth none /home/coder/project
