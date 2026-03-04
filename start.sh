faea552d6879b9603778a6d671b8be4d49c20813
7b809 <ej818793@gmail.com>
2026-03-05 01:23:41 +0530

    Auto update

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+PORT=${PORT:-8080}
 PASSWORD=${PASSWORD:-123456}
 
-echo "Starting code-server..."
-
 code-server \
---bind-addr 0.0.0.0:8080 \
+--bind-addr 0.0.0.0:$PORT \
 --auth password \
 --password $PASSWORD \
 /home/coder/project
